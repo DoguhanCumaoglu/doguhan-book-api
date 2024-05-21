7a15d7405ac07c7a23ee936e010cfab60e0cac83
Doğuhan Cumaoğlu <72949926+DoguhanCumaoglu@users.noreply.github.com>
2024-05-21 21:04:52 +0200

    actions

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -31,7 +31,11 @@ def get_current_user(
     except JWTError:
         raise credentials_exception
     if Token_Session[username] != token:
-        raise credentials_exception
+        credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     user = crud.get_user_by_username(db, username=token_data.username)
     if user is None:
         raise credentials_exception
